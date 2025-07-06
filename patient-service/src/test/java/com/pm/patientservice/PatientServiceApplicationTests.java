35061206a92d260cc1496524d8dc9df30a99bd44
Kishor Bankar <linkekishor@gmail.com>
2025-07-06 14:36:38 +0530

    update

--- patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.pm.patientservice;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-
 import com.pm.patientservice.configuration.TestContainersConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
